f1540a89f3bc41fe3c2ee3d58c4cda73b413fea0
phungngochuyhoang <phungngochuyhoang@gmail.com>
2020-09-08 22:07:46 +0700

    first commit

---
+++ Resources/Js/index.js
@@ -0,0 +1,18 @@
+var index = 0;
+// slide show
+function slide() {
+    var slides = Array.from(document.querySelectorAll('.swiper-slide'));
+    var i;
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    index++;
+    if (index > slides.length) {
+        index = 1;
+    }
+    slides[index-1].style.display = "block";
+    setTimeout(slide, 4000);
+} 
+
+
+
